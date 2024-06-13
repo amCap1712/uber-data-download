4970b99eb3b271d57f9398e6427cf77382836e12
Kartik Ohri <kartikohri13@gmail.com>
2024-06-13 20:25:47 +0530

    add try except

--- backend/app/process_details.py
+++ backend/app/process_details.py
@@ -1,3 +1,4 @@
+import traceback
 from decimal import Decimal
 
 from sqlmodel import Session, select
@@ -12,25 +13,29 @@ def main():
         trips = session.scalars(query)
 
         for trip in trips:
-            trip_dict = trip.details.get("trip", {})
-            receipt_dict = trip.details.get("receipt", {})
+            try:
+                trip_dict = trip.details.get("trip", {})
+                receipt_dict = trip.details.get("receipt", {})
 
-            distance = receipt_dict.get("distance")
-            if distance is not None:
-                trip.distance = Decimal(distance)
-            trip.distance_label = receipt_dict.get("distanceLabel")
+                distance = receipt_dict.get("distance")
+                if distance is not None:
+                    trip.distance = Decimal(distance)
+                trip.distance_label = receipt_dict.get("distanceLabel")
 
-            trip.duration = receipt_dict.get("duration")
-            trip.begin_trip_time = trip_dict.get("beginTripTime")
-            trip.dropoff_trip_time = trip_dict.get("dropoffTime")
+                trip.duration = receipt_dict.get("duration")
+                trip.begin_trip_time = trip_dict.get("beginTripTime")
+                trip.dropoff_trip_time = trip_dict.get("dropoffTime")
 
-            waypoints = trip_dict.get("waypoints", [])
-            if len(waypoints) >= 2:
-                trip.begin_address = waypoints[0]
-                trip.dropoff_address = waypoints[-1]
+                waypoints = trip_dict.get("waypoints", [])
+                if len(waypoints) >= 2:
+                    trip.begin_address = waypoints[0]
+                    trip.dropoff_address = waypoints[-1]
 
-            session.add(trip)
-            session.commit()
+                session.add(trip)
+                session.commit()
+            except Exception:
+                traceback.print_exc()
+                session.rollback()
 
 
 if __name__ == "__main__":
